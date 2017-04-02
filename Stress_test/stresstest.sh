e0b37309e81ea6a08b157896f4797c8b923e2857
Gabry-r <gabryr96@gmail.com>
2017-04-02 04:57:41 +0200

    Modified the stresstest shellscript Added some comments; Added a condition to check if the sys monitor it's open before try to close it; Edited some numbers;

--- Stress_test/stresstest.sh
+++ Stress_test/stresstest.sh
@@ -3,7 +3,7 @@
 
 #####################################
 #stress time-out - default 30m
-stress_time=10s
+stress_time=30m
 #hdd workers - default 2
 hdd=2
 #cpu workers - default 0 (auto)
@@ -22,6 +22,7 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 fi
 
+#warning message
 echo "\n\033[0;31m[WARNING] HIGH CPU/RAM/HDD LOAD"
 echo "USE AT YOUR OWN RISK!"
 echo "Don't leave unsupervised the computer under stress\033[0m\n"
@@ -29,25 +30,34 @@ echo "Don't leave unsupervised the computer under stress\033[0m\n"
 #package check
 dpkg -s stress-ng > /dev/null
 if [ $? != 0 ]; then
+	#if it's not installed then update and install it
 	echo "Stress-ng missing, i will install it for you"
 	sleep 5
-	apt-get update;
-	apt-get install stress-ng;
+	sudo apt-get update;
+	sudo apt-get install stress-ng;
 fi
 
+#print defaults
 echo "Defaults:"
 echo "         Timeout = $stress_time "
 echo "         CPU = $cpu "
 echo "         HDD = $hdd "
 echo "         RAM = $vm * $memory "
+#ask if you want to change these values or not
 echo -n "Do you want to change these values?(Y/n) "
 read answer
+
+#wrong answer flag
 flag=1
+
+#loop in case of wrong answer
 while [ $flag != 0 ]
 do
 	flag=0
 	case "$answer" in
+			#Y or y or enter 
 			"Y"|"y"|"")
+				#read all values
 				echo -n "Enter timeout(s=seconds,m=minutes,h=hours,d=days, ex.10m): "
 				read stress_time
 				echo -n "Enter CPU workers (0=auto, n=disabled, d=default): "
@@ -61,9 +71,11 @@ do
 				echo -n "Enter vm memory (n=disabled, d=default)[m=mega,g=giga, max 1g]: "
 				read memory
 			;;
+			#N or n
 			"N"|"n")
 				echo "Using Defaults!"
 			;;
+			#everything else
 			*)
 				echo -n "Please, enter 'y' or 'n'"
 				flag=1
@@ -71,27 +83,37 @@ do
 	esac
 done
 
+#ask if you want to open a system monitor
 echo "Open a system monitor if you want to see the state of system"
 echo -n "Do you want i try to open 'gnome-system-monitor' for you?(Y/n): "
 read answer
-flag=1
-while [ $flag != 0 ]
+
+#wrong answer flag
+flag=5
+
+#loop in case of wrong answer
+while [ $flag > 2 ]
 do
 	flag=0
 	case "$answer" in
+			#Y or y or enter
 			"Y"|"y"|"")
+				#check if package is installed
 				dpkg -s gnome-system-monitor > /dev/null
+				#if yes run it, else print an error message
 				if [ $? == 0 ]; then
 					sudo killall gnome-system-monitor
 					sudo gnome-system-monitor &
 					echo "Opening..."
 					sleep 10
+					flag=1;
 				else
 					echo "Gnome-system-monitor it's not installed"
 			;;
+			#everything else
 			*)
 				echo -n "Please, enter 'y' or 'n'"
-				flag=1
+				flag=5
 			;;
 	esac
 done
@@ -103,7 +125,11 @@ echo "Wait $stress_time or press ^c (CTRL+C) to abort"
 #stress test
 sudo stress-ng --cpu $cpu --fork $cpu --crypt $cpu --vm $vm  --vm-bytes $memory --hdd $hdd --timeout $stress_time
 
-echo "sys monitor will be closed in 10s (if it's open)"
+#if sys monitor is open then close it after 10s
+if [ $flag == 1 ]; then
+	echo "sys monitor will be closed in 10s"
+	#wait 10s
+	sleep 10
+fi
 
-#wait 10s
-sleep 10
+
