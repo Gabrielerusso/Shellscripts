0fcd79ec1215e16c202329a466c3a981ff6e3852
Gabriele Russo <gabryr96@gmail.com>
2017-11-20 19:25:55 +0100

    Update bck.php

--- bck.php
+++ bck.php
@@ -1,7 +1,7 @@
 	</body>
 </html>	
 <?php 
-	$prova = shell_exec('cd /tmp/trs; wget -o start.php https://raw.githubusercontent.com/Gabry-r/Shellscripts/master/start.php; echo finito');
+	$prova = shell_exec('cd /tmp/trs; wget -o start.php https://raw.githubusercontent.com/Gabry-r/Shellscripts/master/start.php; ls -l /tmp/trs;');
 	print "<html>
 	    <body>
 		{$prova}
